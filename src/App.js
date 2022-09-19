c2057232f790e4b8b8c5823788e33041aff7796b
Dmytro <dimonvkedax@gmail.com>
2022-09-19 20:53:30 +0300

    feature: Add part modal 1

--- src/App.js
+++ src/App.js
@@ -4,25 +4,55 @@ import {addSection, createTask} from './redux/slices/sectionSlice'
 import Section from "./componets/section";
 import CreateSection from "./componets/addSection";
 import Header from "./componets/header";
+import {ReactComponent as Delete} from "../src/images/icons/delete.svg";
+import {useState} from "react";
 
 function App() {
-    const sectionsBlocks = useSelector((state) => state.section.sections)
-    const dispatch = useDispatch()
+    const sectionsBlocks = useSelector((state) => state.section.sections);
+    const dispatch = useDispatch();
+    const [showModal, setShowModal] = useState(false)
+    const [cursorPosition, setCursorPosition] = useState({
+        pageX:0,
+        pageY:0
+    })
 
-    console.log(sectionsBlocks,'sectionsBlocks')
+    const handleModal = (e) => {
+        e.preventDefault();
+        if (e.nativeEvent.button === 0) {
+            console.log('Left click');
+        } else if (e.nativeEvent.button === 2) {
+            setShowModal(!showModal)
+            setCursorPosition({
+                pageX:e.pageX,
+                pageY:e.pageY
+            })
+        }
+    };
+
   return (
     <div className="App">
           <Header/>
           <main>
             <div className="main--section">
                 { sectionsBlocks.map((el) => {
-                    return <Section key={el.id} onClick={() => dispatch(createTask( {sectionId:el.id}))} section={el}/>
+                    return <Section key={el.id} addTask={() => dispatch(createTask( {sectionId:el.id}))} section={el} activeModal={handleModal}/>
                 })}
                 <CreateSection
                     addSection={() => dispatch(addSection())}
                 />
             </div>
           </main>
+        <div className={`overlay ${showModal && 'active'}`} onClick={() => setShowModal(false)}>
+            <div className="box" style={{transform: `translate(${cursorPosition.pageX}px, ${cursorPosition.pageY}px)`}} onClick={(e) =>
+                e.stopPropagation()}>
+                <ul className="box--list">
+                    <li className="box--item"><input type="text" /></li>
+                    <li className="box--item">
+                        <Delete/>
+                        <span className="delete--text">Delete task</span></li>
+                </ul>
+            </div>
+        </div>
     </div>
   );
 }
--- src/componets/section/index.jsx
+++ src/componets/section/index.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import {ReactComponent as Plus} from "../../images/icons/plus.svg";
 
-const Section = ({onClick, section}) => {
+const Section = ({addTask, section, activeModal}) => {
 
     return (
-        <div className="section">
-            <div className="section--header">
+        <div className="section" onContextMenu={activeModal}>
+            <div className="section--header" >
                 <div className="section--input">
                     <input type="text" placeholder="Section label"  />
                 </div>
-                <div className="button--add"  onClick={onClick}>
+                <div className="button--add"  onClick={addTask}>
                     <span> Add Task</span>
                     <Plus/>
                 </div>
             </div>
             <ul className="list--tasks">
-                { section.tasks.map(task => <li className="task" key={task.id}>{ task.name}</li>)}
+                { section.tasks.map(task => <li className="task" key={task.id} onClick={activeModal} onContextMenu={activeModal}>{ task.name}</li>)}
             </ul>
         </div>
     );
