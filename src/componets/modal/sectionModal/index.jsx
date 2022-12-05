23fb858dce35f1063f71032629e9b90ed85ebe3f
Dmytro <dimonvkedax@gmail.com>
2022-12-05 20:51:55 +0200

    feature: add eslint and prettier

--- src/componets/modal/sectionModal/index.jsx
+++ src/componets/modal/sectionModal/index.jsx
@@ -1,51 +1,65 @@
 import React from 'react';
-import ModalWrapper from "../index";
- import {ReactComponent as Delete} from '../../../images/icons/delete.svg';
- import {ReactComponent as Plus} from '../../../images/icons/plus.svg';
-import {COLORS} from "../../../utils/constant";
-import {useDispatch} from "react-redux";
-import {changeColorSection, deleteSection} from "../../../redux/slices/sectionSlice";
-import {useAction} from "../../../hooks/useAction";
+import ModalWrapper from '../index';
+import { ReactComponent as Delete } from '../../../images/icons/delete.svg';
+import { ReactComponent as Plus } from '../../../images/icons/plus.svg';
+import { COLORS } from '../../../utils/constant';
+import { useAction } from '../../../hooks/useAction';
 
-const SectionContextModal = ({addTask,sectionId,sectionName,changeName, color:sectionColor, ...props}) => {
-    const {changeColorSection, deleteSection} = useAction()
+const SectionContextModal = ({
+	addTask,
+	sectionId,
+	sectionName,
+	changeName,
+	color: sectionColor,
+	...props
+}) => {
+	const { changeColorSection, deleteSection } = useAction();
 
-    const handleColor = (color) => {
-        changeColorSection({sectionId:sectionId, sectionColor: color})
-    }
+	const handleColor = (color) => {
+		changeColorSection({ sectionId: sectionId, sectionColor: color });
+	};
 
-    const handleDeleteSection = () => {
-        deleteSection(sectionId)
-    }
+	const handleDeleteSection = () => {
+		deleteSection(sectionId);
+	};
 
-    return (
-        <ModalWrapper {...props}>
-            <ul className="box--list">
-                <li className="box--item" onClick={addTask}>
-                    <Plus/>
-                    <span className="delete--text">Add task</span>
-                </li>
-                <li className="box--item">
-                    <input type="text" value={sectionName} onChange={event => changeName(event.target.value)} />
-                </li>
-                <li className="box--item">
-                    <ul className="colors" >
-                        {COLORS.map((color, idx) => {
-                            return (
-                                <li className={`colors--item ${sectionColor === color && 'active'}`}
-                                    onClick={() => handleColor(color, idx)}
-                                    key={idx} style={{background: `${color}`}}></li>
-                            )
-                        })}
-                    </ul>
-                </li>
-                <li className="box--item" onClick={handleDeleteSection}>
-                    <Delete/>
-                    <span className="delete--text">Delete section</span>
-                </li>
-            </ul>
-        </ModalWrapper>
-    );
+	return (
+		<ModalWrapper {...props}>
+			<ul className="box--list">
+				<li className="box--item" onClick={addTask}>
+					<Plus />
+					<span className="delete--text">Add task</span>
+				</li>
+				<li className="box--item">
+					<input
+						type="text"
+						value={sectionName}
+						onChange={(event) => changeName(event.target.value)}
+					/>
+				</li>
+				<li className="box--item">
+					<ul className="colors">
+						{COLORS.map((color, idx) => {
+							return (
+								<li
+									className={`colors--item ${
+										sectionColor === color && 'active'
+									}`}
+									onClick={() => handleColor(color, idx)}
+									key={idx}
+									style={{ background: `${color}` }}
+								></li>
+							);
+						})}
+					</ul>
+				</li>
+				<li className="box--item" onClick={handleDeleteSection}>
+					<Delete />
+					<span className="delete--text">Delete section</span>
+				</li>
+			</ul>
+		</ModalWrapper>
+	);
 };
 
 export default SectionContextModal;
--- src/componets/taskList/task/index.jsx
+++ src/componets/taskList/task/index.jsx
@@ -1,26 +1,42 @@
-import React, {useEffect, useRef} from 'react';
+import React, { useEffect, useRef } from 'react';
 
-const Task = ({color, section, idx, task,startHandler,dragHandler,endHandler,handleTaskPosition,rightClickTask}) => {
-    const currentTask = useRef()
-    useEffect(() =>{
-        if(!currentTask || !currentTask.current) return
-        handleTaskPosition( currentTask.current)
-    },[currentTask])
-    if(!task) return
+const Task = ({
+	color,
+	section,
+	idx,
+	task,
+	startHandler,
+	dragHandler,
+	endHandler,
+	handleTaskPosition,
+	rightClickTask
+}) => {
+	const currentTask = useRef();
 
-    return (
-        <li className="task"
-            ref={currentTask}
-            style={section && {borderColor: `${section.color}`}}
-            draggable
-            onDragStart={(e) => startHandler(e, task, section.id, idx, currentTask.current)}
-            onDrag={e =>   dragHandler(e)}
-            onDragEnd={e => endHandler(e, currentTask.current)}
-            color={color}
-            onContextMenu={(e) => rightClickTask(e, task.id, task.name)} key={task.id}>
-            { task.name}
-        </li>
-    );
+	useEffect(() => {
+		if (!currentTask || !currentTask.current) return;
+		handleTaskPosition(currentTask.current);
+	}, [currentTask]);
+	if (!task) return;
+
+	return (
+		<li
+			className="task"
+			ref={currentTask}
+			style={section && { borderColor: `${section.color}` }}
+			draggable
+			onDragStart={(e) =>
+				startHandler(e, task, section.id, idx, currentTask.current)
+			}
+			onDrag={(e) => dragHandler(e)}
+			onDragEnd={(e) => endHandler(e, currentTask.current)}
+			color={color}
+			onContextMenu={(e) => rightClickTask(e, task.id, task.name)}
+			key={task.id}
+		>
+			{task.name}
+		</li>
+	);
 };
 
 export default Task;
