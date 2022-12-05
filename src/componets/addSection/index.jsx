35988d34f616612abfb194b3422b47ee37c04bd6
Dmytro <dimonvkedax@gmail.com>
2022-12-05 20:53:59 +0200

    feature: add eslint and prettier 1

--- src/componets/addSection/index.jsx
+++ src/componets/addSection/index.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import {ReactComponent as Plus} from "../../images/icons/plus.svg";
+import { ReactComponent as Plus } from '../../images/icons/plus.svg';
 
-const CreateSection = ({addSection}) => {
-    return (
-        <div className="add--section" onClick={addSection} >
-            <span>Add section</span>
-            <Plus/>
-        </div>
-    );
+const CreateSection = ({ addSection }) => {
+	return (
+		<div className="add--section" onClick={addSection}>
+			<span>Add section</span>
+			<Plus />
+		</div>
+	);
 };
 
 export default CreateSection;
